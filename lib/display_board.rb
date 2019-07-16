e304154107bf954d04e9780ebf72811a60e26d02
Elizabeth Smith <elizabeth.smith1121@gmail.com>
2019-07-16 23:37:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+def display_board (X , O)
+  puts board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+>>>>>>> 6642223250faa916ad90c58416265a3c27a10b1e
 end
